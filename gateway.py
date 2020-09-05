685d4aa7f2f127b64479c4b5fd1560c9c27d199b
Denis De Mesmaeker <denis.demesmaeker@gmail.com>
2020-09-05 13:40:46 +0200

    -node id's now support 16bit id's to just use the device id - gateway receives data as 4*16bit data junks

--- gateway.py
+++ gateway.py
@@ -21,10 +21,33 @@ try:
         while not radio.receiveDone():
             time.sleep(.1)
 
-        print("'%s' from %s RSSI:%s" % ("".join([chr(letter) for letter in radio.DATA]), radio.SENDERID, radio.RSSI))
+        """
+        print("'{data}' from 0x{sender:04x}. RSSI: {rssi}".format(
+            data="".join([chr(letter) for letter in radio.DATA]),
+            sender=radio.SENDERID,
+            rssi=radio.RSSI)
+        )
+        """
+
+        # data conists of chunks of 4 bytes: 2 bytes cmd name, 2 bytes value
+        i = 0
+        data = {}
+        while i < len(radio.DATA):
+            cmd = "".join(chr(letter) for letter in radio.DATA[i:i + 2])
+            value = (radio.DATA[i + 2] << 8) | radio.DATA[i + 3]
+            i += 4
+            data[cmd] = value
+
         sender = radio.SENDERID
+        rssi = radio.RSSI
+
         if radio.ACKRequested():
-            radio.sendACK(sender, dt.now().strftime("%Y-%m-%d %H:%M:%S"))  # return the current datestamp to the sender
+            radio.sendACK(sender, dt.now().strftime("%Y-%m-%d %H:%M:%S"))  # return the current timestamp to the sender
+
+        # process received data
+        print("data from 0x{sender:2x} [RSSI: {rssi}]".format(sender=sender, rssi=rssi))
+        for k, v in data.items():
+            print("{k}: {v}".format(k=k, v=v))
 
 except KeyboardInterrupt:
     radio.shutdown()
